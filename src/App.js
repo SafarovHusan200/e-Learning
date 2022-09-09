a793e8f5791c51a3e28cb52b488c8f11a7a07284
SafarovHusan200 <safarovhusan2000@gmail.com>
2022-09-09 07:17:58 +0500

    AboutSection html complated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+// import pages 
+import AboutUs from './pages/AboutUs'
 
 function App() {
   return (
     <div>
       <h1>eLearning</h1>
-     
+      <AboutUs />
     </div>
   )
 }
